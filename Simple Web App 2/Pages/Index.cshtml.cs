1abab20bbac837d630debfcd35060a3b75c495b0
godeepsheep <godeepsheep@protonmail.com>
2025-01-29 09:02:32 +0100

    fdsf
    dsfsd

--- Simple Web App 2/Pages/Index.cshtml.cs
+++ Simple Web App 2/Pages/Index.cshtml.cs
@@ -9,12 +9,42 @@ public class IndexModel : PageModel
   public string? Text { get; set; }
   
   [BindProperty]
-  public int? Number { get; set; }
+  public int? Number { get; set; } //Set default value to 1 if you are short on time. It prevents issues that I had to deal with.
   
   [BindProperty]
-  public bool AddSpace { get; set; }
+  public bool AddSpace { get; set; } //Bind didnt work for me. So I had to manually bind it in the csHTML
 
-   
+  public string Result { get; set; } = string.Empty;
+
+  public void OnPost()
+  {
+      if (string.IsNullOrEmpty(Text))
+      {
+          Result = "Enter some text.";
+          return; //Important to do returns or it will continue to the next if statement. If both statements fail, it will only show the last error message. 
+          //Return ensures the code stops if there is an error. 
+      }
+
+      if (Number == null || Number == 0)
+      {
+          Result = "Enter a positive number."; 
+          return;
+      }
+      
+      // Debugging: Print AddSpace value
+      Console.WriteLine($"Checkbox value: {AddSpace}");
+
+      if (AddSpace)
+      {
+          Result = string.Join(" ", Enumerable.Repeat(Text, Number.Value)); //We use.Value because int? allows nullable. So we extract the value if there is one.
+          //If it was just int, then you wouldn't need the .Value
+          //If value is null then it will cause an error. You could avoid it by giving a value of 1 or 0 in your properties since 0 isn't null.
+      }
+      else
+      {
+          Result = string.Concat(Enumerable.Repeat(Text, Number.Value)); //Concat joins multiple strings together without seperator.
+      }
+  }
 
     public void OnGet()
     {
--- Simple Web App 3/obj/Debug/net8.0/Simple Web App 3.AssemblyInfo.cs
+++ Simple Web App 3/obj/Debug/net8.0/Simple Web App 3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Simple Web App 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8ac8af186fe89a02ef3ff7eaf9310e4c6e4cbeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4407b432108dfe908b04aefea27d576d6008f4d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Simple Web App 3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Simple Web App 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
